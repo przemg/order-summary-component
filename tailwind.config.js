f2059f7ef994d839aabf49644f7ec6cb30e5b78b
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-08-24 18:09:59 +0200

    feat: finish css

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: 'hsl(225, 100%, 94%)',
+          200: 'hsl(245, 75%, 52%)'
+        },
+        neutral: {
+          100: 'hsl(225, 100%, 98%)',
+          200: 'hsl(224, 23%, 55%)',
+          300: 'hsl(223, 47%, 23%)'
+        }
+      },
+      fontFamily: {
+        body: ['Red Hat Display']
+      },
+      boxShadow: {
+        blue: '0 16px 32px -12px hsla(245, 75%, 52%, 0.64)'
+      },
+      fontWeight: {
+        regular: 500,
+        medium: 700,
+        black: 900
+      },
+      backgroundImage: {
+        bgMobile: "url('/assets/images/pattern-background-mobile.svg')",
+        bgDesktop: "url('/assets/images/pattern-background-desktop.svg')", 
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
